2ecf9974270bf923e458c1fe83f58ec762e24898
Ocraz <registered@quicknet.nl>
2019-12-20 18:22:51 +0100

    Add files via upload

---
+++ splitxld.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from tldextract import extract
+import signal
+import argparse
+
+def signal_handler(sig, frame):
+        print("\nCtrl-C detected, exiting...\n")
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("format", help="Each number from 1 to 9 is replaced with the corresponding domain level taken from the input (takes vTLD such as co.uk into account). For example, if the argument 3.2.1 is given and stdin supplies sub5.sub4.sub3.example.co.uk then sub3.example.co.uk is returned. The dots are free-form, any character can be used.")
+args = parser.parse_args()
+
+for sInFqdn in sys.stdin:
+    sOutput = args.format
+    # Because of vTLD suffixes:
+    dl3, dl2, dl1 = extract(sInFqdn)
+    sInFqdn = sInFqdn.rsplit(dl1, 1)[0]
+    sOutput = sOutput.replace(str(1),dl1)
+    
+    aFqdnSplit = sInFqdn.strip().split(".")
+    
+    i = len(aFqdnSplit)
+    for sDomainLevel in aFqdnSplit:
+        i -= 1
+        if i > 9:
+            continue
+        sOutput = sOutput.replace(str(i+1),sDomainLevel)
+    
+    sys.stdout.write(sOutput + "\n")
