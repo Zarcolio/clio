ab0f7fc1bcc000f52ab391859ff3af42c2c456e5
Ocraz <registered@quicknet.nl>
2020-01-06 22:12:34 +0100

    Add files via upload

--- 2cmd.py
+++ 2cmd.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 import signal
 import os
+from termcolor import colored
 
 def signal_handler(sig, frame):
         print("\nCtrl-C detected, exiting...\n")
@@ -22,6 +23,7 @@ signal.signal(signal.SIGINT, signal_handler)
 # Get some commandline arguments:
 parser = argparse.ArgumentParser(description="This script takes input lines from stdin and inserts them in the commands provided in the commands file. This way you can execute a certain command many times. For example you can take screen shots of URLs with cutycapt provided by output of another command.")
 parser.add_argument("cmd", help="File containing one or more commands that should be executed. Use $2cmd$ or $2cmdsan$ in lowercase in each command line. $2cmd$ is replaced with each line from input. Use $cmdsan$ to sanitize a string for use in a filename.")
+parser.add_argument("-v", "--verbose", help="In red, show the commands that are created from stdin and the provide config file.", action="store_true")
 args = parser.parse_args()
 
 if args.cmd:
@@ -50,5 +52,6 @@ for strInput in sys.stdin:
         sCmd = sCmd.replace("$2cmd$", strInput, len(sCmd))
         #sCmd = sCmd.strip()
         sCmd = sCmd.replace("\n", "", len(sCmd))
-        #print (sCmd)
+        if args.verbose:
+            sys.stderr.write((colored(sCmd,"red"))+"\n")
         os.system(sCmd)
