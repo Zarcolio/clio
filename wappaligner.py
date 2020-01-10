ab621c7d51c0cb6e36db3b0949c5cbb88172a334
Ocraz <registered@quicknet.nl>
2020-01-10 11:10:34 +0100

    Add files via upload

--- wappaligner.py
+++ wappaligner.py
@@ -3,6 +3,7 @@
 
 import json
 import sys
+import urllib.parse
 from termcolor import colored
 
 sStandardInput = ""
@@ -11,16 +12,26 @@ for x in sys.stdin:
     sStandardInput = sStandardInput + x 
 
 #print (sStandardInput)
+try:
+    json_data = json.loads(sStandardInput)
+except:
+    pass
 
-json_data = json.loads(sStandardInput)
 
-#print (json_data["urls"])
+for x in json_data["urls"]:
+    print (x)
+    
 for sUrl in json_data["urls"]:
     print (colored("URL = " + sUrl,"green"))
+#    print (colored("URL = " + sUrl["status"],"green"))
 
 for app in json_data["applications"]:
     if app["version"]:
-        sVersion = " " + str(app["version"])
+        sVersion1 = " " + str(app["version"])
+        sVersion2 = " \"" + str(app["version"]) + "\""  
     else:
-        sVersion = ""
-    print (str(app["name"]) + sVersion + " (" + app["confidence"] + "%) - " + app["website"])
+        sVersion1 = ""
+        sVersion2 = ""
+
+    sGoogleUrl = "https://www.google.com/search?q=" + urllib.parse.quote("\"" + app["name"] + "\"" + sVersion2 + " cve | exploit | vulnerability")
+    print (app["name"] + sVersion1 + " (" + app["confidence"] + "%) - " + app["website"] + " - " + sGoogleUrl)
