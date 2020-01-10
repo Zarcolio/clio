c9a06813a76ffaa424f3dc03c94c1d4513ba8f2f
Ocraz <registered@quicknet.nl>
2020-01-10 13:25:26 +0100

    Add files via upload

--- 2cmd.py
+++ 2cmd.py
@@ -32,7 +32,7 @@ signal.signal(signal.SIGINT, signal_handler)
 parser = argparse.ArgumentParser(description="This script takes input lines from stdin and inserts them in the commands provided in the commands file. This way you can execute a certain command many times. For example you can take screen shots of URLs with cutycapt provided by output of another command.")
 parser.add_argument("cmd", help="File containing one or more commands that should be executed. Use $2cmd$ or $2cmdsan$ in lowercase in each command line. $2cmd$ is replaced with each line from input. Use $cmdsan$ to sanitize a string for use in a filename.")
 parser.add_argument("-t", "--timeout", help="Wait x milliseconds between commands.")
-parser.add_argument("-v", "--verbose", help="In red, show the commands that are created from stdin and the provide config file.", action="store_true")
+parser.add_argument("-v", "--verbose", help="In green, show the commands that are created from stdin and the provide config file.", action="store_true")
 args = parser.parse_args()
 
 if args.cmd:
