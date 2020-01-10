bf611bdd50e10da7f23042a674aa010fae89219f
Ocraz <registered@quicknet.nl>
2020-01-10 12:25:52 +0100

    Add files via upload

--- 2cmd.py
+++ 2cmd.py
@@ -6,11 +6,11 @@ import argparse
 import signal
 import os
 from termcolor import colored
+import time
 
 def signal_handler(sig, frame):
         print("\nCtrl-C detected, exiting...\n")
         sys.exit(0)
-
 
 def FileNameSan(sFileName):
     sFileName = sFileName.replace("://", "-")
@@ -18,11 +18,20 @@ def FileNameSan(sFileName):
     sFileName = sFileName.replace("/", "-")
     return sFileName
 
+def escapeString(sString):
+    sString = sString.replace("$","\$", len(sString))
+    sString = sString.replace("&","\&", len(sString))
+    sString = sString.replace(";","\;", len(sString))
+    sString = sString.replace("\"","\\\"", len(sString))
+    #sString = sString.replace("","", len(sString))
+    return sString
+
 signal.signal(signal.SIGINT, signal_handler)
 
 # Get some commandline arguments:
 parser = argparse.ArgumentParser(description="This script takes input lines from stdin and inserts them in the commands provided in the commands file. This way you can execute a certain command many times. For example you can take screen shots of URLs with cutycapt provided by output of another command.")
 parser.add_argument("cmd", help="File containing one or more commands that should be executed. Use $2cmd$ or $2cmdsan$ in lowercase in each command line. $2cmd$ is replaced with each line from input. Use $cmdsan$ to sanitize a string for use in a filename.")
+parser.add_argument("-t", "--timeout", help="Wait x milliseconds between commands.")
 parser.add_argument("-v", "--verbose", help="In red, show the commands that are created from stdin and the provide config file.", action="store_true")
 args = parser.parse_args()
 
@@ -39,8 +48,15 @@ try:
 except FileNotFoundError:
     print ("File not found, exiting...")
     sys.exit(1)
+
+iFirst = 0
     
 for strInput in sys.stdin:
+    if args.timeout:
+        if iFirst == 1:
+            time.sleep (int(args.timeout)/1000)
+        iFirst = 1  
+        
     for sCmd in aCmds:
         sCmd = sCmd.strip()
         if not strInput.strip():
@@ -49,9 +65,13 @@ for strInput in sys.stdin:
         strInputSan = FileNameSan(strInput)
         sCmd = sCmd.replace("$2cmdsan$", strInputSan, len(sCmd))
         #sCmd = sCmd.strip()
+
         sCmd = sCmd.replace("$2cmd$", strInput, len(sCmd))
         #sCmd = sCmd.strip()
+
         sCmd = sCmd.replace("\n", "", len(sCmd))
         if args.verbose:
             sys.stderr.write((colored(sCmd,"green"))+"\n")
+
+        sCmd = escapeString(sCmd)
         os.system(sCmd)
