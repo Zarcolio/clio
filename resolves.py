3e7c652da599640a6e361cd497c72e8247dd8064
Zarcolio <registered@quicknet.nl>
2020-02-24 17:11:46 +0100

    Add files via upload

--- resolves.py
+++ resolves.py
@@ -8,7 +8,11 @@ import argparse
 import dns.resolver
 from random import choice
 from string import ascii_letters, digits
+import pyasn
+import os
+from tldextract import extract
 
+asndb = pyasn.pyasn(os.path.dirname(os.path.realpath(sys.argv[0])) + '/asn.db')
 
 def signal_handler(sig, frame):
 	sys.stderr.write("\nCtrl-C detected, quitting...\n")
@@ -45,17 +49,21 @@ parser.add_argument("-nc", "--nocname", help="Ommit FQDN from printing to the sc
 parser.add_argument("-os", "--success", help="Define an output CSV file for succesfully resolved FQDNs. This file will contain the hostnames found with their corresponding IP addresses.")
 parser.add_argument("-of", "--failed", help="Define an output file for FQDNs that failed to resolve.")
 parser.add_argument("-op", "--plain", help="Define an output file for succesfully resolved FQDNs. This file will contain only hostnames found.")
+parser.add_argument("-osr", "--soarecord", help="Define an output file for SOA records found.")
 
 args = parser.parse_args()
 
 if args.plain:
-	fPlain = open(args.plain, 'w')
+	fPlain = open(args.plain, 'w', buffering=1)
 
 if args.success:
-	fSuccess = open(args.success, 'w')
+	fSuccess = open(args.success, 'w', buffering=1)
 
 if args.failed:
-	fFailed = open(args.failed, 'w')
+	fFailed = open(args.failed, 'w', buffering=1)
+
+if args.soarecord:
+	fSoa = open(args.soarecord, 'w', buffering=1)
 
 sRandom =  ''.join([choice(ascii_letters + digits) for i in range(20)])
 dRembemberdIpAddressesRandom = {}
@@ -79,9 +87,6 @@ for strInput in sys.stdin:
             if args.plain:
                 fPlain.write(strInput + " \n")
         dRembemberdIpAddressesRandom[str(dIpAddressesRandom)]=True
-        #print (dIpAddressesRandom)
-        #if sIpAddressRandom in dIpAddresses:
-        #    print ("Wildcard detected:" + sIpAddressRandom + " for " + strInput)
     else:
         if not dCnames or not args.nocname:
             sys.stdout.write(strInput + "\n")
@@ -93,11 +98,21 @@ for strInput in sys.stdin:
     if args.success:
         if dCnames:
             for sCname in dCnames: 
-                #fSuccess.write(strInput + ";" + sIpAddress + " \n")
                 fSuccess.write(strInput + ";" + sCname[:-1] + " \n")
         else:
             if dIpAddresses:
                 for sIpAddress in dIpAddresses: 
-                    fSuccess.write(strInput + ";" + sIpAddress + " \n")
+                    sAsn = asndb.lookup(sIpAddress)
+                    fSuccess.write(strInput + ";" + sIpAddress  + ";" + str(sAsn) + " \n")
                 
-    
+    if args.soarecord:
+        dl3, dl2, dl1 = extract(strInput)
+        dltotal = dl2 + "." + dl1
+        for dl in reversed(dl3.strip().split(".")):
+            dltotal = dl + "." + dltotal
+            try:
+                answers = dns.resolver.query(dltotal, 'SOA')
+                fSoa.write(dltotal + " \n")
+                
+            except:
+                pass
