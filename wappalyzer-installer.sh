c3b26341b0e5f7f35005c1520f25b14e583ec97a
Zarcolio <registered@quicknet.nl>
2020-03-28 23:59:28 +0100

    Add files via upload

--- wappalyzer-installer.sh
+++ wappalyzer-installer.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 sudo apt-get install -y nodejs npm
-npm i wappalyzer-cli
+
+
+npm i -g commander
+npm i -g normalize-url
+npm i -g url
+npm i -g wappalyzer
+npm i -g puppeteer
+npm i -g wappalyzer-cli
+
